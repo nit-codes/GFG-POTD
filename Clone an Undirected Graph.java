e136353bea4d1e9f03c61f470ea5b25355314aed
NITHIN B Y <137317447+nit-codes@users.noreply.github.com>
2024-02-14 21:42:40 +0530

    Create Clone an Undirected Graph.java

---
+++ Clone an Undirected Graph.java
@@ -0,0 +1,45 @@
+//User function Template for Java
+
+
+/*
+    class Node{
+        int val;
+        ArrayList<Node> neighbors;
+        public Node(){
+            val = 0;
+            neighbors = new ArrayList<>();
+        }
+    
+        public Node(int val){
+            this.val = val;
+            neighbors = new ArrayList<>();
+        }
+    
+        public Node(int val, ArrayList<Node> neighbors){
+            this.val = val;
+            this.neighbors = neighbors;
+        }
+    }
+*/
+class Solution{
+    
+        static void dfs(Node ans,Node node,HashMap<Integer,Node> vis){
+        vis.put(ans.val,ans);
+        for(Node x:node.neighbors){
+            if(vis.get(x.val)==null){
+                Node newNode = new Node(x.val);
+                ans.neighbors.add(newNode);
+                dfs(newNode,x,vis);
+            }
+            else{
+                ans.neighbors.add(vis.get(x.val));
+            }
+        }
+    }
+    Node cloneGraph(Node node){
+        Node ans = new Node(node.val);
+        HashMap<Integer,Node> vis = new HashMap<>();
+        dfs(ans,node,vis);
+        return ans;
+    }
+}
