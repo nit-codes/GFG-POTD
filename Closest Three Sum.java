25455966ac9fe64cde6253a9c4f90c4cb30485fb
NITHIN B Y <137317447+nit-codes@users.noreply.github.com>
2024-07-09 22:35:53 +0530

    Create Closest Three Sum.java

---
+++ Closest Three Sum.java
@@ -0,0 +1,24 @@
+
+    static int threeSumClosest(int[] array, int target) {
+        // code here
+        int diff=Integer.MAX_VALUE,ans=0;
+        Arrays.sort(array);
+        for(int i=0;i<array.length-2;i++){
+            int j=i+1,k=array.length-1;
+            while(j<k){
+                int sum=array[i]+array[j]+array[k];
+                int d = Math.abs(sum-target);
+                if(d<diff){
+                    diff=d;
+                    ans=sum;
+                }
+                else if(d==diff){
+                    ans=Math.max(ans,sum);
+                }
+                if(sum<target)j++;
+                else if(sum==target)return sum;
+                else k--;
+            }
+        }
+        return ans;
+    }
