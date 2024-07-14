4f7d87952e91a331bd301a0534f982c1e6544614
NITHIN B Y <137317447+nit-codes@users.noreply.github.com>
2024-07-14 20:37:14 +0530

    Create Segregate 0s and 1s.java

---
+++ Segregate 0s and 1s.java
@@ -0,0 +1,13 @@
+void segregate0and1(int[] arr) {
+        // code here        
+        int zero=0,one=arr.length-1;
+        while(zero<one){
+            if(arr[zero]==1){
+                int temp=arr[zero];
+                arr[zero]=arr[one];
+                arr[one]=temp;
+                one--;
+            }
+            else zero++;
+        }
+    }
