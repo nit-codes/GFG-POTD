1bbebc4beb3bc503706fd69d8ba96b1ae71aad2f
NITHIN B Y <137317447+nit-codes@users.noreply.github.com>
2024-07-17 21:50:29 +0530

    Create Construct Binary Tree from Parent Array.java

---
+++ Construct Binary Tree from Parent Array.java
@@ -0,0 +1,21 @@
+class Solution {
+    // Function to construct binary tree from parent array.
+    public Node createTree(int parent[]) {
+        // Your code here
+        int n = parent.length;
+        Node temp[] = new Node[n];
+        for(int i=0;i<n;i++){
+            temp[i]=new Node(i);
+        }
+        Node head=null;
+        for(int i=0;i<n;i++){
+            if(parent[i]!=-1){
+                if(temp[parent[i]].left==null)temp[parent[i]].left=temp[i];
+                else temp[parent[i]].right=temp[i];
+            }
+            else head=temp[i];
+        }
+        return head;
+    }
+
+}
