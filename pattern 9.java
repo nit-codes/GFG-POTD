5a295a58b59e9893449cd70f2a6c2d5f8faa43df
NITHIN B Y <137317447+nit-codes@users.noreply.github.com>
2024-12-11 13:10:59 +0530

    Create pattern 9.java

---
+++ pattern 9.java
@@ -0,0 +1,13 @@
+class Solution{
+  static void(int n){
+    for(int i=0;i<n;i++){
+      for(int j=0;j<i;j++){
+        System.out.print(" ");
+      }
+      for(int k=0;k< 2*n-(2*i+1);k++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  } 
+}
